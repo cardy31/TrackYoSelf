f7422f8eca8980d9071ba4f166fb900832e1b61a
Rob Cardy <rob.cardy.31@gmail.com>
2017-01-22 10:25:39 -0500

    ALL DONE!

--- templates/stats-view.php
+++ templates/stats-view.php
@@ -1,14 +1,19 @@
 <body ng-app="Stats">
   <div class="container-fluid">
-
-    <div class="jumbotron text-center">
-      <h1>Track Yo Stats!</h1>
-      <h2>Here you can find your food diary</h2>
+    <div id="cont">
+        <div class="jumbotron text-center">
+            <h1>Track Yo Stats!</h1>
+            <h2>Here you can find your food diary</h2>
+        </div>
     </div>
 
+
     <div ng-controller="mainCtrl">
-      <div class="row" ng-repeat="data in datas"  ng-hide="data.highres==='localhost'">
-        <div class="col-md-6" >
+      <div class="row" id="rob" ng-repeat="data in datas"  ng-hide="data.highres==='localhost'">
+          <div class="col-md-1">
+
+          </div>
+        <div class="col-md-5" >
           <h3>Nutritional Facts</h3>
           <p><strong>Name:</strong> {{data.name}}</p>
           <p><strong>Brand Name:</strong> {{data.brandName}}</p>
@@ -22,14 +27,15 @@
           <p><strong>Cholesterol:</strong> {{data.cholesterol}}</p>
           <p><strong>Sodium:</strong> {{data.sodium}}</p>
         </div>
+          <div class="col-md-1">
 
-        <div class="col-md-6">
+          </div>
+
+        <div class="col-md-5">
           <h3>Product Image</h3>
           <img src = {{data.highres}} height="250" width="250">
         </div>
-
     </div>
-
 
     </div>
   </div>
