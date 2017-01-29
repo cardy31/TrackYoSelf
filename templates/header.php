0204f0e4a74d0dc385a7363db1a90ab2eb26a252
Rob Cardy <rob.cardy.31@gmail.com>
2017-01-28 21:45:17 -0500

    ALL DONE!

--- templates/header.php
+++ templates/header.php
@@ -9,7 +9,6 @@
 <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-<script src="js/app.js"></script>
 <link href="https://fonts.googleapis.com/css?family=Roboto:100" rel="stylesheet">
 <link rel="stylesheet" href="/css/bootstrap.css">
 <link rel="stylesheet" href="/css/style.css">
